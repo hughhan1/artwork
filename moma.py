6417c1b22a7eb5d533245e76ec52418e5c42f221
Hugh Han <hughhan1@gmail.com>
2017-04-18 18:43:19 -0400

    MoMA Thumbnail Scraping with -t or --thumbnails Option
    To scrape thumbnails from the MoMA collection, run the original
    scraper (moma.py) with either the -t or --thumbnails option.

--- moma.py
+++ moma.py
@@ -5,12 +5,15 @@ the artworks.json file.
 
 
 from bs4 import BeautifulSoup
-import json
+from urllib  import urlretrieve
 from urllib2 import urlopen
-import urllib
+
+import json
+import sys
 
 
 image_dir     = 'images'
+thumb_dir     = 'thumbnails'
 artworks_file = 'json/artworks.json'
 
 
@@ -45,9 +48,39 @@ def get_image(url, filename):
     image_tag  = soup.find('meta', property='og:image', content=True)
     image_link = image_tag['content']
 
-    print('Downloading %s to %s directory.' % (filename, image_dir))
-    urllib.urlretrieve(image_link, image_dir + '/' + filename)
+    if image_link == "":
+        print('error   : no image found at .' % (url))
+    else:
+        try:
+            urlretrieve(image_link, image_dir + '/' + filename)
+            print('success : %s downdloaded to %s directory.' % (filename, image_dir))
+        except AttributeError:
+            print('error   : %s could not be downloaded to %s directory.' % (filename, image_dir))
     return image_link
+
+
+def get_thumbnail(url, filename):
+    """
+    Retrieves the thumbnail associated with the URL provided, and writes it
+    using the filename provided.
+
+    Args:
+        url      : the URL with information about a particular piece of artwork
+        filename : the filename to which the thumbnail should be saved
+
+    Returns:
+        a string containing the href link to the thumbnail that was written
+    """
+
+    if url == "":
+        print('error: url cannot be an empty string.' % (filename, thumb_dir))
+    else:
+        try:
+            urlretrieve(url, thumb_dir + '/' + filename)
+            print('success : %s downdloaded to %s directory.' % (filename, thumb_dir))
+        except AttributeError:
+            print('error   : %s could not be downloaded to %s directory.' % (filename, thumb_dir))
+    return url
 
 
 def get_images(artworks_filename):
@@ -69,7 +102,31 @@ def get_images(artworks_filename):
     artworks_file.close()
 
 
+def get_thumbnails(artworks_filename):
+    """
+    Iterates through a JSON data file, and retrieves all thumbnails associated
+    with it, and downloads them to a thumbnail directory
+
+    Args:
+        artworks_filename : the filename of the JSON data file
+    """
+    artworks_file = open(artworks_filename)
+    artworks_data = json.load(artworks_file)
+    
+    for artwork in artworks_data:
+        url       = artwork['ThumbnailURL']
+        object_id = artwork['ObjectID']
+        get_thumbnail(url, str(object_id) + '.jpg')
+    
+    artworks_file.close()
+
+
 def main():
+    if sys.argv > 1:
+        if sys.argv[1] == '-t' or sys.argv[1] == '--thumbnails':    
+            get_thumbnails(artworks_file)
+        else:
+            raise Exception
     get_images(artworks_file)
 
 

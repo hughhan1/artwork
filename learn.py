62a6daacdd86544336e34f960740a64e80286199
David Li <zdavidli@users.noreply.github.com>
2017-04-18 19:13:03 -0400

    learn.py

---
+++ learn.py
@@ -0,0 +1,8 @@
+from sklearn import datasets
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+OneVsRestClassifier(LinearSVC(random_state=0)).fit(X, y).predict(X)

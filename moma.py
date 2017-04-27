03b92988f638d4197f5594b78bb7e7dc55ff3b70
Benjamin Hoertnagl-Pereira <bhoertn1@jhu.edu>
2017-04-27 18:56:45 -0400

    Added filtering of images by nationality and url timeout

--- moma.py
+++ moma.py
@@ -11,11 +11,15 @@ from urllib2 import urlopen
 import json
 import os
 import sys
+import csv
 
 
 image_dir     = 'images'
 thumb_dir     = 'thumbnails'
 artworks_file = 'json/artworks.json'
+
+pad = 6 #padding to ensure filnames are 6 characters, for sorting in utils.py
+max_imgs = 600 #TODO: pass as argument
 
 
 def make_soup(url):
@@ -49,14 +53,19 @@ def get_image(url, filename):
     image_tag  = soup.find('meta', property='og:image', content=True)
     image_link = image_tag['content']
 
+
     if image_link == "":
         print('error   : no image found at .' % (url))
     else:
+
+
         try:
-            urlretrieve(image_link, os.path.join(image_dir, filename))
-            print('success : %s downloaded to %s directory.' % (filename, image_dir))
-        except AttributeError:
+            request = urlopen(image_link, timeout=5) #timeout for getting image
+            with open(os.path.join(image_dir, filename), 'wb') as f:
+                f.write(request.read())
+        except:
             print('error   : %s could not be downloaded to %s directory.' % (filename, image_dir))
+
     return image_link
 
 
@@ -83,6 +92,24 @@ def get_thumbnail(url, filename):
             print('error   : %s could not be downloaded to %s directory.' % (filename, thumb_dir))
     return url
 
+def write_labels(nation_labels):
+    """
+    Creates CSV file of labels (nationality, date) to be used by utils.py
+
+    Args:
+        nation_labels : the nationalities
+        date_labels   : the artwork dates
+    """
+
+    print("French: "+ str(nation_labels.count("French")))
+    print("British: "+ str(nation_labels.count("British")))
+    print("American: "+ str(nation_labels.count("American")))
+    nation_csv = "nations.csv"
+    with open(nation_csv, "w") as output:
+        writer = csv.writer(output, lineterminator='\n')
+        for val in nation_labels:
+            writer.writerow([val])
+
 
 def get_images(artworks_filename):
     """
@@ -95,12 +122,25 @@ def get_images(artworks_filename):
     artworks_file = open(artworks_filename)
     artworks_data = json.load(artworks_file)
     
-    for artwork in artworks_data:
+    nation_labels = []
+    i = 0
+    for artwork in artworks_data[:max_imgs]:
         url       = artwork['URL']
         object_id = artwork['ObjectID']
-        if url is not None:
-            get_image(url, str(object_id) + '.jpg')
+        nation    = artwork['Nationality']
+
+        print(str(object_id))
+        if url is not None and nation and object_id != 209 and object_id != 304 and object_id != 345 and object_id != 361:
+
+            check = ["American", "British", "Italian"] #TODO: pass as command line
+            #ensure that nationality is known remove: ["", "Nationality unknown", "Nationality Unknown"] 
+            if nation[0] in check:
+                get_image(url, str(object_id).zfill(pad) + '.jpg')
+                nation_labels.append(nation[0]) #choose first nationality, sometimes repeated
+
+                print(str(object_id) + " : " + nation[0])
     
+    write_labels(nation_labels)
     artworks_file.close()
 
 
@@ -118,7 +158,7 @@ def get_thumbnails(artworks_filename):
     for artwork in artworks_data:
         url       = artwork['ThumbnailURL']
         object_id = artwork['ObjectID']
-        get_thumbnail(url, str(object_id) + '.jpg')
+        get_thumbnail(url, str(object_id).zfill(pad) + '.jpg')
     
     artworks_file.close()
 
